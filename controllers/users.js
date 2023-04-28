554a2ae1d4577d95d1eea1e28550697d1f353a6d
Adgamov Artur <adgamoffartur@yandex.ru>
2023-04-28 14:19:08 +0300

    Добавлено: обработка ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -8,6 +8,11 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserId = (req, res) => {
+  if (req.params.userId) {
+    res.status(404).send('Пользователь по указанному _id не найден.');
+    return;
+  }
+
   // ищет запись по идентификатору
   User.findById(req.params.userId)
     .then((user) => res.send(user))
@@ -18,6 +23,19 @@ module.exports.createUser = (req, res) => {
   // ищет запись по идентификатору
   const { name, about, avatar } = req.body;
 
+  if (!name) {
+    res.status(400).send('Переданы некорректные данные при создании пользователя.');
+    return;
+  }
+  if (!about) {
+    res.status(400).send('Переданы некорректные данные при создании пользователя.');
+    return;
+  }
+  if (!avatar) {
+    res.status(400).send('Переданы некорректные данные при создании пользователя.');
+    return;
+  }
+
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
     .catch((err) => res.status(500).send(`Произошла ошибка ${err.name}: ${err.message}`));
@@ -26,6 +44,21 @@ module.exports.createUser = (req, res) => {
 module.exports.updateUserInfo = (req, res) => {
   const userId = req.user._id;
   const { name, about } = req.body;
+
+  if (!name) {
+    res.status(400).send('Переданы некорректные данные при обновлении профиля.');
+    return;
+  }
+  if (!about) {
+    res.status(400).send('Переданы некорректные данные при обновлении профиля.');
+    return;
+  }
+
+  if (userId) {
+    res.status(404).send('Пользователь по указанному _id не найден.');
+    return;
+  }
+
   User.findByIdAndUpdate(
     userId,
     { name, about },
@@ -41,6 +74,17 @@ module.exports.updateUserInfo = (req, res) => {
 module.exports.updateUserAvatar = (req, res) => {
   const userId = req.user._id;
   const { avatar } = req.body;
+
+  if (!avatar) {
+    res.status(400).send('Переданы некорректные данные при обновлении профиля.');
+    return;
+  }
+
+  if (userId) {
+    res.status(404).send('Пользователь по указанному _id не найден.');
+    return;
+  }
+
   User.findByIdAndUpdate(
     userId,
     { avatar },
