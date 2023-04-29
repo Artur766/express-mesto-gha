71775072fd3e13c265177a4d8a7a27104d4c6430
Adgamov Artur <adgamoffartur@yandex.ru>
2023-04-29 17:42:03 +0300

    22

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ module.exports.getCards = (req, res) => {
   Card.find({})
     .populate(['owner', 'likes'])
     .then((cards) => res.send(cards))
-    .catch((err) => res.status(500).send(`Произошла ошибка ${err.name}: ${err.message}`));
+    .catch(() => res.status(500).send({ message: 'Произошла внутренняя ошибка сервера.' }));
 };
 
 module.exports.createCard = (req, res) => {
@@ -15,14 +15,20 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner: id })
     .then((card) => card.populate('owner'))
     .then((card) => res.send(card))
-    .catch((err) => res.status(500).send(`Произошла ошибка ${err.name}: ${err.message}`));
+    .catch((err) => {
+      if (err.name === 'ValidationError') return res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      return res.status(500).send({ message: 'Произошла внутренняя ошибка сервера.' });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   // Удаление конкретной записи
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send(`Произошла ошибка ${err.name}: ${err.message}`));
+    .then((card) => {
+      if (!card) return res.status(404).send({ message: 'Карточка с указанным id не найдена.' });
+      return res.send(card);
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла внутренняя ошибка сервера.' }));
 };
 
 module.exports.likeCard = (req, res) => {
@@ -31,8 +37,14 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send(`Произошла ошибка ${err.name}: ${err.message}`));
+    .then((card) => {
+      if (!card) return res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
+      return res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') return res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка.' });
+      return res.status(500).send({ message: 'Произошла внутренняя ошибка сервера.' });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -41,6 +53,12 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send(`Произошла ошибка ${err.name}: ${err.message}`));
+    .then((card) => {
+      if (!card) return res.status(404).send({ message: 'Пользователь с указанным id не найден.' });
+      return res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') return res.status(400).send({ message: 'Переданы некорректные данные для снятии лайка.' });
+      return res.status(500).send({ message: 'Произошла внутренняя ошибка сервера.' });
+    });
 };
